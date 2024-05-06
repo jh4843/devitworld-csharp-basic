082dd0d1b92b54cfd3814df9ad1a533d0334ce85
김재혁 <2017387@vieworks.com>
2024-05-07 08:55:00 +0900

    Update Program.cs

--- DevitworldConsoleApp/5_Class/Program.cs
+++ DevitworldConsoleApp/5_Class/Program.cs
@@ -4,24 +4,25 @@ namespace ClassOOP
 {
     public class Animal
     {
-        // 필드
+        // 필드 (Field)
         public string Name;
         private int _age;
 
-        // 생성자
+        // 생성자 (Constructor)
         public Animal(string name, int age)
         {
             Name = name;
             _age = age;
         }
 
+        // 속성 (Property)
         public int Age
         {
             get { return _age; }
-            set { _age = value > 0 ? value : 0; } // 나이는 0 이상만 가능
+            set { _age = value > 0 ? value : 0; } // Age is set to 0 if value is lower than 0
         }
 
-        // 메서드
+        // 메서드 (Method)
         public void Speak()
         {
             Console.WriteLine("This animal makes a sound.");
