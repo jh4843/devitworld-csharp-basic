ed0a3f4c6f2930b3a3961970dbee1ed4c025ed85
jh4843 <creamboy1@naver.com>
2024-05-13 23:08:50 +0900

    Add 8. Database Proejct

---
+++ DevitworldConsoleApp/8_Database/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace UseDatabase
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
